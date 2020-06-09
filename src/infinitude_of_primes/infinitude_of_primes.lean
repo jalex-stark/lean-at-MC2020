c7248df329d4e70ce9d637d786f6fabcfaca5330
Apurva-MacbookAir <apurvnakade@gmail.com>
2020-06-09 09:45:22 -0400

    added infinitude of primes proof

---
+++ src/infinitude_of_primes/infinitude_of_primes.lean
@@ -0,0 +1,21 @@
+import tactic
+import data.nat.prime
+
+open nat
+
+theorem exists_infinite_primes (n : ℕ) : ∃ p, p ≥ n ∧ prime p :=          
+begin
+  let p:= min_fac (n.fact + 1), use p,
+  have pp : p.prime, from min_fac_prime (ne_of_gt (succ_lt_succ (fact_pos n))),
+ 
+  split,
+  show p.prime, from pp,
+ 
+  show p ≥ n,
+  begin
+    by_contradiction hp,
+    have hp₁: p ∣ n.fact, from dvd_fact (min_fac_pos (n.fact + 1)) (le_of_not_ge hp),
+    have hp₂: p ∣ 1, from (nat.dvd_add_iff_right hp₁).2 (fact n + 1).min_fac_dvd,
+    exact pp.not_dvd_one hp₂,
+  end,
+end
